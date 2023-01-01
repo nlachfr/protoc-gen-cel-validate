60bf6e74293f3636e316ad42e1075f05033d734a
Nicolas Lach <dev@nlach.fr>
2023-01-01 22:12:53 +0100

    Update testdata generation

--- testdata/cmd/plugin/method.pb.go
+++ testdata/cmd/plugin/method.pb.go
@@ -7,7 +7,6 @@
 package plugin
 
 import (
-	_ "github.com/Neakxs/protocel/validate"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
