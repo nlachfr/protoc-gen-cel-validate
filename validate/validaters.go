0589e50ed82b182bd871654d3e98e7618ceae705
Neakxs <dev@nlach.fr>
2023-01-15 22:34:31 +0100

    Add validate overloads fallback for proto messages (#21)
    * Rewrite validaters with fallback
    
    * Add tests

--- validate/validaters.go
+++ validate/validaters.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/Neakxs/protocel/validate/errors"
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/common/types"
 	"github.com/google/cel-go/common/types/ref"
@@ -13,68 +12,44 @@ import (
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 )
 
-func buildValidateFunctionOpt(name string, t string) cel.FunctionOpt {
-	return cel.MemberOverload(
-		fmt.Sprintf("%s_%s", t, name),
-		[]*cel.Type{cel.ObjectType(t)},
-		cel.BoolType,
-		cel.UnaryBinding(func(value ref.Val) ref.Val {
-			if v, ok := value.Value().(Validater); ok {
-				if err := v.Validate(context.TODO()); err == nil {
-					return types.Bool(true)
-				} else if vErr, ok := err.(errors.ValidateError); ok {
-					return vErr
-				} else {
-					return types.NewErr(err.Error())
-				}
-			}
-			return types.Bool(false)
-		}),
-	)
-}
-
-func buildValidateWithMaskFunctionOpt(name string, t string) cel.FunctionOpt {
-	return cel.MemberOverload(
-		fmt.Sprintf("%s_%s", t, name),
-		[]*cel.Type{cel.ObjectType(t), cel.ObjectType(string((&fieldmaskpb.FieldMask{}).ProtoReflect().Descriptor().FullName()))},
-		cel.BoolType,
-		cel.BinaryBinding(func(lhs, rhs ref.Val) ref.Val {
-			if v, ok := lhs.Value().(Validater); ok {
-				if fm, ok := rhs.Value().(*fieldmaskpb.FieldMask); ok {
-					if err := v.ValidateWithMask(context.TODO(), fm); err == nil {
-						return types.Bool(true)
-					} else if vErr, ok := err.(errors.ValidateError); ok {
-						return vErr
-					} else {
-						return types.NewErr(err.Error())
-					}
-				}
-			}
-			return types.Bool(false)
-		}),
-	)
-}
-
-func buildValidatersFunctions(desc protoreflect.MessageDescriptor) []cel.EnvOption {
+func buildValidatersFunctions(config *ValidateOptions, desc protoreflect.MessageDescriptor, envOpt cel.EnvOption, imports ...protoreflect.FileDescriptor) []cel.EnvOption {
 	res := []cel.EnvOption{}
-	if r := buildValidaterFunction("validate", buildValidateFunctionOpt, desc); r != nil {
+	builder := &validateOverloadBuilder{
+		config:  config,
+		envOpt:  envOpt,
+		imports: imports,
+	}
+	if r := builder.buildValidateFunction(desc); r != nil {
 		res = append(res, r)
 	}
-	if r := buildValidaterFunction("validateWithMask", buildValidateWithMaskFunctionOpt, desc); r != nil {
+	if r := builder.buildValidateWithMaskFunction(desc); r != nil {
 		res = append(res, r)
 	}
 	return res
 }
 
-func buildValidaterFunction(name string, optBuilder func(name string, t string) cel.FunctionOpt, desc protoreflect.MessageDescriptor) cel.EnvOption {
-	opts := buildValidaterFunctionOpts(name, optBuilder, desc, map[string]bool{})
-	if len(opts) > 0 {
-		return cel.Function(name, opts...)
+type validateOverloadBuilder struct {
+	config   *ValidateOptions
+	envOpt   cel.EnvOption
+	imports  []protoreflect.FileDescriptor
+	fallback map[string]MessageValidateProgram
+}
+
+func (b *validateOverloadBuilder) buildValidateFunction(desc protoreflect.MessageDescriptor) cel.EnvOption {
+	if opts := b.buildFunctionOpts(desc, "validate", b.ValidateFunctionOpt, map[string]bool{}); len(opts) > 0 {
+		return cel.Function("validate", opts...)
 	}
 	return nil
 }
 
-func buildValidaterFunctionOpts(name string, optBuilder func(name string, t string) cel.FunctionOpt, desc protoreflect.MessageDescriptor, m map[string]bool) []cel.FunctionOpt {
+func (b *validateOverloadBuilder) buildValidateWithMaskFunction(desc protoreflect.MessageDescriptor) cel.EnvOption {
+	if opts := b.buildFunctionOpts(desc, "validateWithMask", b.ValidateWithMaskFunctionOpt, map[string]bool{}); len(opts) > 0 {
+		return cel.Function("validateWithMask", opts...)
+	}
+	return nil
+}
+
+func (b *validateOverloadBuilder) buildFunctionOpts(desc protoreflect.MessageDescriptor, name string, optBuilder func(name, t string) cel.FunctionOpt, m map[string]bool) []cel.FunctionOpt {
 	functionOpts := []cel.FunctionOpt{}
 	messageType := string(desc.FullName())
 	if _, ok := m[messageType]; !ok {
@@ -89,12 +64,85 @@ func buildValidaterFunctionOpts(name string, optBuilder func(name string, t stri
 				buildValidate = true
 			}
 			if fd.Kind() == protoreflect.MessageKind {
-				functionOpts = append(functionOpts, buildValidaterFunctionOpts(name, optBuilder, fd.Message(), m)...)
+				functionOpts = append(functionOpts, b.buildFunctionOpts(fd.Message(), name, optBuilder, m)...)
 			}
 		}
 		if buildValidate {
 			functionOpts = append(functionOpts, optBuilder(name, messageType))
 		}
 	}
+	fmt.Println(m)
 	return functionOpts
+}
+
+func (b *validateOverloadBuilder) ValidateFunctionOpt(name, t string) cel.FunctionOpt {
+	return cel.MemberOverload(
+		fmt.Sprintf("%s_%s", t, name),
+		[]*cel.Type{cel.ObjectType(t)},
+		cel.BoolType,
+		cel.UnaryBinding(func(value ref.Val) ref.Val {
+			var err error
+			if v, ok := value.Value().(Validater); ok {
+				err = v.Validate(context.TODO())
+			} else if msg, ok := value.Value().(proto.Message); ok {
+				desc := msg.ProtoReflect().Descriptor()
+				pgr, ok := b.fallback[string(desc.FullName())]
+				if !ok {
+					if fbPgr, err := BuildMessageValidateProgram(b.config, desc, b.envOpt, b.imports...); err != nil {
+						return types.NewErr(err.Error())
+					} else {
+						pgr = fbPgr
+					}
+				}
+				err = pgr.ValidateWithMask(context.TODO(), msg, &fieldmaskpb.FieldMask{Paths: []string{"*"}})
+			} else {
+				return types.Bool(false)
+			}
+			if err == nil {
+				return types.Bool(true)
+			} else if vErr, ok := err.(ref.Val); ok {
+				return vErr
+			} else {
+				return types.NewErr(err.Error())
+			}
+		}),
+	)
+}
+
+func (b *validateOverloadBuilder) ValidateWithMaskFunctionOpt(name, t string) cel.FunctionOpt {
+	return cel.MemberOverload(
+		fmt.Sprintf("%s_%s", t, name),
+		[]*cel.Type{cel.ObjectType(t), cel.ObjectType(string((&fieldmaskpb.FieldMask{}).ProtoReflect().Descriptor().FullName()))},
+		cel.BoolType,
+		cel.BinaryBinding(func(lhs, rhs ref.Val) ref.Val {
+			var err error
+			fm, ok := rhs.Value().(*fieldmaskpb.FieldMask)
+			if !ok {
+				return types.Bool(false)
+			}
+			if v, ok := lhs.Value().(Validater); ok {
+				err = v.ValidateWithMask(context.TODO(), fm)
+			} else if msg, ok := lhs.Value().(proto.Message); ok {
+				desc := msg.ProtoReflect().Descriptor()
+				pgr, ok := b.fallback[string(desc.FullName())]
+				if !ok {
+					if fbPgr, err := BuildMessageValidateProgram(b.config, desc, b.envOpt, b.imports...); err != nil {
+						return types.NewErr(err.Error())
+					} else {
+						pgr = fbPgr
+					}
+				}
+				err = pgr.ValidateWithMask(context.TODO(), msg, fm)
+			} else {
+				return types.Bool(false)
+			}
+			if err == nil {
+				return types.Bool(true)
+			} else if vErr, ok := err.(ref.Val); ok {
+				return vErr
+			} else {
+				return types.NewErr(err.Error())
+			}
+		}),
+	)
 }
--- validate/validaters_test.go
+++ validate/validaters_test.go
@@ -1,90 +1,127 @@
 package validate
 
 import (
-	"fmt"
-	"reflect"
+	"context"
 	"testing"
 
 	"github.com/Neakxs/protocel/testdata/validate"
 	"github.com/google/cel-go/cel"
 	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/types/known/structpb"
-	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
 )
 
-type optBuilder struct {
-	got  map[string]bool
-	want map[string]bool
+var tests = []struct {
+	Name           string
+	Message        proto.Message
+	Config         *ValidateOptions
+	EnvOpt         cel.EnvOption
+	WantCompileErr bool
+	WantEvalErr    bool
+	WantFalse      bool
+}{
+	{
+		Name:        "NOK (Validater implemented)",
+		Message:     &validate.TestRpcRequest{},
+		WantEvalErr: true,
+	},
+	{
+		Name:        "NOK (Validater not implemented)",
+		Message:     &validate.FieldExpr{Name: "name"},
+		WantEvalErr: true,
+	},
+	{
+		Name:    "OK (Validater not implemented)",
+		Message: &validate.FieldExpr{Name: "notname"},
+	},
+	{
+		Name:    "OK (Validater implemented)",
+		Message: &validate.TestRpcRequest{Ref: "refs/myref", Raw: "raw"},
+	},
 }
 
-func (b *optBuilder) Build(name string, t string) cel.FunctionOpt {
-	if b.got == nil {
-		b.got = map[string]bool{}
-	}
-	b.got[t] = true
-	return buildValidateFunctionOpt(name, t)
-}
-
-func (b *optBuilder) Error() error {
-	if len(b.got) == 0 && len(b.want) == 0 {
-		return nil
-	} else if !reflect.DeepEqual(b.got, b.want) {
-		return fmt.Errorf("want %v, got %v", b.want, b.got)
-	}
-	return nil
-}
-
-func TestBuildValidaterFunctionOpts(t *testing.T) {
-	tests := []struct {
-		Name    string
-		Message proto.Message
-		Builder *optBuilder
-	}{
-		{
-			Name:    "Message without rules",
-			Message: &timestamppb.Timestamp{},
-			Builder: &optBuilder{want: map[string]bool{}},
-		},
-		{
-			Name:    "Field rules",
-			Message: &validate.FieldExpr{},
-			Builder: &optBuilder{want: map[string]bool{
-				string((&validate.FieldExpr{}).ProtoReflect().Descriptor().FullName()): true,
-			}},
-		},
-		{
-			Name:    "Message rule",
-			Message: &validate.MessageExpr{},
-			Builder: &optBuilder{want: map[string]bool{
-				string((&validate.MessageExpr{}).ProtoReflect().Descriptor().FullName()): true,
-			}},
-		},
-		{
-			Name:    "Message with rules, and nested message with rules",
-			Message: &validate.MessageNestedExpr{},
-			Builder: &optBuilder{want: map[string]bool{
-				string((&validate.MessageNestedExpr{}).ProtoReflect().Descriptor().FullName()): true,
-				string((&validate.MessageExpr{}).ProtoReflect().Descriptor().FullName()):       true,
-			}},
-		},
-		{
-			Name:    "Message without rules, and nested message with rules",
-			Message: &validate.MessageNested{},
-			Builder: &optBuilder{want: map[string]bool{
-				string((&validate.MessageExpr{}).ProtoReflect().Descriptor().FullName()): true,
-			}},
-		},
-		{
-			Name:    "Recursive message",
-			Message: &structpb.Struct{},
-			Builder: &optBuilder{want: map[string]bool{}},
-		},
-	}
+func TestValidateFunctionOpt(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
-			buildValidaterFunctionOpts("validate", tt.Builder.Build, tt.Message.ProtoReflect().Descriptor(), map[string]bool{})
-			if err := tt.Builder.Error(); err != nil {
+			env, err := cel.NewEnv(
+				cel.TypeDescs(tt.Message.ProtoReflect().Descriptor().ParentFile()),
+				cel.Variable("myvar", cel.ObjectType(string(tt.Message.ProtoReflect().Descriptor().FullName()))),
+				(&validateOverloadBuilder{
+					config: tt.Config,
+					envOpt: tt.EnvOpt,
+				}).buildValidateFunction(tt.Message.ProtoReflect().Descriptor()),
+			)
+			if err != nil {
 				t.Error(err)
+			}
+			ast, issues := env.Compile(`myvar.validate()`)
+			if issues != nil {
+				err = issues.Err()
+			}
+			if (tt.WantCompileErr && err == nil) || (!tt.WantCompileErr && err != nil) {
+				t.Errorf("wantCompileErr %v, got %v", tt.WantCompileErr, err)
+			}
+			pgr, err := env.Program(ast)
+			if err != nil {
+				t.Error(err)
+			} else {
+				val, _, err := pgr.ContextEval(context.Background(), map[string]interface{}{"myvar": tt.Message})
+				if err == nil {
+					if e, ok := val.Value().(error); ok {
+						err = e
+					}
+				}
+				if (tt.WantEvalErr && err == nil) || (!tt.WantEvalErr && err != nil) {
+					t.Errorf("wantEvalErr %v, got %v", tt.WantEvalErr, err)
+				} else if err == nil {
+					if (tt.WantFalse && val.Value().(bool)) || (!tt.WantFalse && !val.Value().(bool)) {
+						t.Errorf("wantFalse %v, got %v", tt.WantFalse, val.Value().(bool))
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestValidateWithMaskFunctionOpt(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			env, err := cel.NewEnv(
+				cel.TypeDescs(tt.Message.ProtoReflect().Descriptor().ParentFile()),
+				cel.TypeDescs(fieldmaskpb.File_google_protobuf_field_mask_proto),
+				cel.Variable("myvar", cel.ObjectType(string(tt.Message.ProtoReflect().Descriptor().FullName()))),
+				cel.Variable("fm", cel.ObjectType(string((&fieldmaskpb.FieldMask{}).ProtoReflect().Descriptor().FullName()))),
+				(&validateOverloadBuilder{
+					config: tt.Config,
+					envOpt: tt.EnvOpt,
+				}).buildValidateWithMaskFunction(tt.Message.ProtoReflect().Descriptor()),
+			)
+			if err != nil {
+				t.Error(err)
+			}
+			ast, issues := env.Compile(`myvar.validateWithMask(fm)`)
+			if issues != nil {
+				err = issues.Err()
+			}
+			if (tt.WantCompileErr && err == nil) || (!tt.WantCompileErr && err != nil) {
+				t.Errorf("wantCompileErr %v, got %v", tt.WantCompileErr, err)
+			}
+			pgr, err := env.Program(ast)
+			if err != nil {
+				t.Error(err)
+			} else {
+				val, _, err := pgr.ContextEval(context.Background(), map[string]interface{}{"myvar": tt.Message, "fm": &fieldmaskpb.FieldMask{Paths: []string{"*"}}})
+				if err == nil {
+					if e, ok := val.Value().(error); ok {
+						err = e
+					}
+				}
+				if (tt.WantEvalErr && err == nil) || (!tt.WantEvalErr && err != nil) {
+					t.Errorf("wantEvalErr %v, got %v", tt.WantEvalErr, err)
+				} else if err == nil {
+					if (tt.WantFalse && val.Value().(bool)) || (!tt.WantFalse && !val.Value().(bool)) {
+						t.Errorf("wantFalse %v, got %v", tt.WantFalse, val.Value().(bool))
+					}
+				}
 			}
 		})
 	}
