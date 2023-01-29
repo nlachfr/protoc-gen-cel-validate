ffd4ab04ac58832f17373bc789123add646f6da3
Nicolas Lach <dev@nlach.fr>
2023-01-29 17:34:24 +0100

    Merge options.proto with validate.proto

--- validate/options.go
+++ validate/options.go
@@ -1,14 +1,148 @@
-package options
+package validate
 
 import (
 	"fmt"
 
 	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/checker"
 	"github.com/google/cel-go/checker/decls"
 	"github.com/google/cel-go/common"
+	"github.com/google/cel-go/interpreter/functions"
 	"github.com/google/cel-go/parser"
 	v1alpha1 "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 )
+
+type Library struct {
+	EnvOpts []cel.EnvOption
+	PgrOpts []cel.ProgramOption
+}
+
+func (l *Library) CompileOptions() []cel.EnvOption     { return l.EnvOpts }
+func (l *Library) ProgramOptions() []cel.ProgramOption { return l.PgrOpts }
+
+func BuildEnvOption(options *Options, descs ...protoreflect.MessageDescriptor) cel.EnvOption {
+	if options != nil {
+		decls := []*v1alpha1.Decl{}
+		if options.Globals != nil {
+			decls = append(decls, buildDeclsFromOptionsGlobals(options.Globals)...)
+		}
+		if options.Overloads != nil {
+			decls = append(decls, buildDeclsFromOptionsOverloads(options.Overloads)...)
+		}
+		reservedNames := map[string]bool{}
+		if options.StdlibOverridingEnabled {
+			if options.Globals != nil {
+				for k := range options.Globals.Constants {
+					reservedNames[k] = true
+				}
+				for k := range options.Globals.Functions {
+					reservedNames[k] = true
+				}
+			}
+			if options.Overloads != nil {
+				for k := range options.Overloads.Functions {
+					reservedNames[k] = true
+				}
+				for k := range options.Overloads.Variables {
+					reservedNames[k] = true
+				}
+			}
+			for _, desc := range descs {
+				if desc != nil {
+					for i := 0; i < desc.Fields().Len(); i++ {
+						reservedNames[desc.Fields().Get(i).TextName()] = true
+					}
+				}
+			}
+		}
+		for _, decl := range checker.StandardDeclarations() {
+			if _, ok := reservedNames[decl.Name]; !ok {
+				decls = append(decls, decl)
+			}
+		}
+		return cel.Lib(&Library{
+			EnvOpts: []cel.EnvOption{cel.Declarations(decls...), cel.Macros(cel.StandardMacros...)},
+			PgrOpts: []cel.ProgramOption{cel.Functions(functions.StandardOverloads()...)},
+		})
+	}
+	return cel.StdLib()
+}
+
+func buildDeclsFromOptionsGlobals(globals *Options_Globals) []*v1alpha1.Decl {
+	dcls := []*v1alpha1.Decl{}
+	if globals != nil {
+		for k, v := range globals.Constants {
+			dcls = append(dcls, decls.NewConst(
+				k,
+				decls.String,
+				&v1alpha1.Constant{ConstantKind: &v1alpha1.Constant_StringValue{StringValue: v}},
+			))
+		}
+	}
+	return dcls
+}
+
+func buildDeclsFromOptionsOverloads(overloads *Options_Overloads) []*v1alpha1.Decl {
+	dcls := []*v1alpha1.Decl{}
+	if overloads != nil {
+		for name, v := range overloads.Functions {
+			args := []*v1alpha1.Type{}
+			overload := name
+			for i := 0; i < len(v.Args); i++ {
+				args = append(args, TypeFromOverloadType(v.Args[i]))
+			}
+			dcls = append(dcls, decls.NewFunction(
+				name, decls.NewOverload(
+					overload,
+					args,
+					TypeFromOverloadType(v.Result),
+				),
+			))
+		}
+		for k, v := range overloads.Variables {
+			dcls = append(dcls, decls.NewVar(k, TypeFromOverloadType(v)))
+		}
+	}
+	return dcls
+}
+
+func TypeFromOverloadType(t *Options_Overloads_Type) *v1alpha1.Type {
+	switch v := t.Type.(type) {
+	case *Options_Overloads_Type_Primitive_:
+		switch v.Primitive {
+		case Options_Overloads_Type_BOOL:
+			return decls.Bool
+		case Options_Overloads_Type_INT:
+			return decls.Int
+		case Options_Overloads_Type_UINT:
+			return decls.Uint
+		case Options_Overloads_Type_DOUBLE:
+			return decls.Double
+		case Options_Overloads_Type_BYTES:
+			return decls.Bytes
+		case Options_Overloads_Type_STRING:
+			return decls.String
+		case Options_Overloads_Type_DURATION:
+			return decls.Duration
+		case Options_Overloads_Type_TIMESTAMP:
+			return decls.Timestamp
+		case Options_Overloads_Type_ERROR:
+			return decls.Error
+		case Options_Overloads_Type_DYN:
+			return decls.Dyn
+		case Options_Overloads_Type_ANY:
+			return decls.Any
+		}
+	case *Options_Overloads_Type_Object:
+		return decls.NewObjectType(v.Object)
+	case *Options_Overloads_Type_Array_:
+		return decls.NewListType(TypeFromOverloadType(v.Array.Type))
+	case *Options_Overloads_Type_Map_:
+		return decls.NewMapType(TypeFromOverloadType(v.Map.Key), TypeFromOverloadType(v.Map.Value))
+	}
+	return decls.Null
+}
 
 func BuildMacros(options *Options, expr string, envOpts []cel.EnvOption) ([]parser.Macro, error) {
 	macros := []parser.Macro{}
--- validate/options_test.go
+++ validate/options_test.go
@@ -1,15 +1,324 @@
-package options
+package validate
 
 import (
 	"testing"
 
 	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/checker/decls"
 	"github.com/google/cel-go/common"
 	"github.com/google/cel-go/common/operators"
+	"github.com/google/cel-go/common/types"
+	"github.com/google/cel-go/common/types/ref"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	v1alpha1 "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/testing/protocmp"
 )
+
+func TestBuildEnvOption(t *testing.T) {
+	tests := []struct {
+		Name    string
+		Expr    string
+		Desc    protoreflect.MessageDescriptor
+		EnvOpt  cel.EnvOption
+		Config  *Options
+		WantErr bool
+	}{
+		{
+			Name:    "No options",
+			Expr:    "1 == 2",
+			WantErr: false,
+		},
+		{
+			Name:    "Constant (error)",
+			Expr:    `name == "name"`,
+			WantErr: true,
+		},
+		{
+			Name: "Constant",
+			Expr: `name == "name"`,
+			Config: &Options{
+				Globals: &Options_Globals{
+					Constants: map[string]string{"name": "name"},
+				},
+			},
+			WantErr: false,
+		},
+		{
+			Name: "Overload variable (error)",
+			Expr: `name == "name"`,
+			Config: &Options{
+				Overloads: &Options_Overloads{
+					Variables: map[string]*Options_Overloads_Type{"name": {
+						Type: &Options_Overloads_Type_Primitive_{Primitive: Options_Overloads_Type_STRING}},
+					},
+				},
+			},
+			WantErr: true,
+		},
+		{
+			Name: "Overload variable",
+			Expr: `name == "name"`,
+			Config: &Options{
+				Overloads: &Options_Overloads{
+					Variables: map[string]*Options_Overloads_Type{"name": {
+						Type: &Options_Overloads_Type_Primitive_{Primitive: Options_Overloads_Type_STRING}},
+					},
+				},
+			},
+			EnvOpt: cel.Lib(&Library{
+				PgrOpts: []cel.ProgramOption{cel.Globals(map[string]interface{}{"name": "name"})},
+			}),
+			WantErr: false,
+		},
+		{
+			Name: "Overload function (error)",
+			Expr: `getName() == "name"`,
+			Config: &Options{
+				Overloads: &Options_Overloads{
+					Functions: map[string]*Options_Overloads_Function{
+						"getName()": {Result: &Options_Overloads_Type{Type: &Options_Overloads_Type_Primitive_{Primitive: Options_Overloads_Type_STRING}}},
+					},
+				},
+			},
+			WantErr: true,
+		},
+		{
+			Name: "Overload function",
+			Expr: `getName() == "name"`,
+			Config: &Options{
+				Overloads: &Options_Overloads{
+					Functions: map[string]*Options_Overloads_Function{
+						"getName": {Result: &Options_Overloads_Type{Type: &Options_Overloads_Type_Primitive_{Primitive: Options_Overloads_Type_STRING}}},
+					},
+				},
+			},
+			EnvOpt: cel.Lib(&Library{
+				EnvOpts: []cel.EnvOption{cel.Function("getName", cel.Overload("getName", []*cel.Type{}, cel.StringType, cel.FunctionBinding(func(values ...ref.Val) ref.Val { return types.String("name") })))},
+				PgrOpts: []cel.ProgramOption{},
+			}),
+			WantErr: false,
+		},
+		{
+			Name:    "Stdlib override (error)",
+			Expr:    `type == "type"`,
+			WantErr: true,
+		},
+		{
+			Name: "Stdlib override (const)",
+			Expr: `type == "type"`,
+			Config: &Options{
+				Globals: &Options_Globals{
+					Constants: map[string]string{"type": "name"},
+				},
+				StdlibOverridingEnabled: true,
+			},
+			WantErr: false,
+		},
+		{
+			Name: "Stdlib override (variable)",
+			Expr: `type == "type"`,
+			Config: &Options{
+				Overloads: &Options_Overloads{
+					Variables: map[string]*Options_Overloads_Type{"type": {
+						Type: &Options_Overloads_Type_Primitive_{Primitive: Options_Overloads_Type_STRING}},
+					},
+				},
+				StdlibOverridingEnabled: true,
+			},
+			EnvOpt: cel.Lib(&Library{
+				PgrOpts: []cel.ProgramOption{cel.Globals(map[string]interface{}{"type": "name"})},
+			}),
+			WantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			opts := []cel.EnvOption{BuildEnvOption(tt.Config, tt.Desc)}
+			if tt.EnvOpt != nil {
+				opts = append(opts, tt.EnvOpt)
+			}
+			env, err := cel.NewCustomEnv(opts...)
+			if err == nil {
+				ast, issues := env.Compile(tt.Expr)
+				if issues != nil {
+					err = issues.Err()
+				} else {
+					pgr, perr := env.Program(ast, cel.EvalOptions(cel.OptOptimize))
+					if perr != nil {
+						err = perr
+					} else {
+						_, _, err = pgr.Eval(map[string]interface{}{})
+					}
+				}
+			}
+			if (err != nil && !tt.WantErr) || (err == nil && tt.WantErr) {
+				t.Errorf("wantErr %v, got %v", tt.WantErr, err)
+			}
+		})
+	}
+}
+
+func TestTypeFromOverloadType(t *testing.T) {
+	tests := []struct {
+		Name string
+		In   *Options_Overloads_Type
+		Out  *v1alpha1.Type
+	}{
+		{
+			Name: "Primitive bool",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Primitive_{
+					Primitive: Options_Overloads_Type_BOOL,
+				},
+			},
+			Out: decls.Bool,
+		},
+		{
+			Name: "Primitive int",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Primitive_{
+					Primitive: Options_Overloads_Type_INT,
+				},
+			},
+			Out: decls.Int,
+		},
+		{
+			Name: "Primitive uint",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Primitive_{
+					Primitive: Options_Overloads_Type_UINT,
+				},
+			},
+			Out: decls.Uint,
+		},
+		{
+			Name: "Primitive double",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Primitive_{
+					Primitive: Options_Overloads_Type_DOUBLE,
+				},
+			},
+			Out: decls.Double,
+		},
+		{
+			Name: "Primitive bytes",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Primitive_{
+					Primitive: Options_Overloads_Type_BYTES,
+				},
+			},
+			Out: decls.Bytes,
+		},
+		{
+			Name: "Primitive string",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Primitive_{
+					Primitive: Options_Overloads_Type_STRING,
+				},
+			},
+			Out: decls.String,
+		},
+		{
+			Name: "Primitive duration",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Primitive_{
+					Primitive: Options_Overloads_Type_DURATION,
+				},
+			},
+			Out: decls.Duration,
+		},
+		{
+			Name: "Primitive timestamp",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Primitive_{
+					Primitive: Options_Overloads_Type_TIMESTAMP,
+				},
+			},
+			Out: decls.Timestamp,
+		},
+		{
+			Name: "Primitive error",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Primitive_{
+					Primitive: Options_Overloads_Type_ERROR,
+				},
+			},
+			Out: decls.Error,
+		},
+		{
+			Name: "Primitive dyn",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Primitive_{
+					Primitive: Options_Overloads_Type_DYN,
+				},
+			},
+			Out: decls.Dyn,
+		},
+		{
+			Name: "Primitive any",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Primitive_{
+					Primitive: Options_Overloads_Type_ANY,
+				},
+			},
+			Out: decls.Any,
+		},
+		{
+			Name: "Object",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Object{
+					Object: "object",
+				},
+			},
+			Out: decls.NewObjectType("object"),
+		},
+		{
+			Name: "Array",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Array_{
+					Array: &Options_Overloads_Type_Array{
+						Type: &Options_Overloads_Type{
+							Type: &Options_Overloads_Type_Primitive_{
+								Primitive: Options_Overloads_Type_BOOL,
+							},
+						},
+					},
+				},
+			},
+			Out: decls.NewListType(decls.Bool),
+		},
+		{
+			Name: "Map",
+			In: &Options_Overloads_Type{
+				Type: &Options_Overloads_Type_Map_{
+					Map: &Options_Overloads_Type_Map{
+						Key: &Options_Overloads_Type{
+							Type: &Options_Overloads_Type_Primitive_{
+								Primitive: Options_Overloads_Type_STRING,
+							},
+						},
+						Value: &Options_Overloads_Type{
+							Type: &Options_Overloads_Type_Primitive_{
+								Primitive: Options_Overloads_Type_STRING,
+							},
+						},
+					},
+				},
+			},
+			Out: decls.NewMapType(decls.String, decls.String),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			res := TypeFromOverloadType(tt.In)
+			if !cmp.Equal(res, tt.Out, protocmp.Transform()) {
+				t.Errorf("want %v, got %v", tt.Out, res)
+			}
+		})
+	}
+}
 
 func TestBuildMacros(t *testing.T) {
 	tests := []struct {
